b7653d6b38afadaebdd9f0380268ee717f6d71ee
Shifath Salam <43546293+sheef-code@users.noreply.github.com>
2021-05-16 23:41:39 -0700

    created weather table

--- pages/weather.js
+++ pages/weather.js
@@ -1,40 +1,57 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
-import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+import axios from "axios";
 
-import Layout from '../components/layout';
+import Layout from "../components/layout";
 
 const Weather = () => {
-    const[data, setData] = useState([]);
-    
-    useEffect(() => {
-        axios.get('https://agile-beach-69479.herokuapp.com/')
-        .then(function (response) {
-            // handle success
-            console.log(response.data);
-            setData(response.data);
-          })
-          .catch(function (error) {
-            // handle error
-            console.log(error);
-          })})  
-    
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://agile-beach-69479.herokuapp.com/")
+      .then(function (response) {
+        // handle success
+        console.log(response.data);
+        response.data.shift();
+        setData(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  });
+
   return (
-      <>  
+    <>
       <Layout title="Weather">
-          <h2>Weather!</h2>
-            <table>
-                <tbody>
-                    {data.map((item) => (
-                        <tr>{item.date} {item.maxTempF} {item.minTempF} {item.maxTempC} {item.minTempC}</tr> ))}
-                </tbody>
-            </table>
-       </Layout>    
-      </>
+        <table className="weather-table">
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Max Temp °F</th>
+              <th>Min Temp °F</th>
+              <th>Max Temp °C</th>
+              <th>Min Temp °C</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr>
+                <td>{item.date}</td>
+                <td>{item.maxTempF}</td>
+                <td>{item.minTempF}</td>
+                <td>{item.maxTempC}</td>
+                <td>{item.minTempC}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Layout>
+    </>
   );
-  
-}
+};
 export default dynamic(() => Promise.resolve(Weather), {
-    ssr: false
-  })
+  ssr: false,
+});
